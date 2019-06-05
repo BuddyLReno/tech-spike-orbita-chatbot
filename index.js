85ee945e4fdea6f93c3b68eac884b496dd59dd53
Buddy Reno <buddy.reno@daveramsey.com>
2019-06-05 09:54:46 -0500

    Add cors package for express

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const cors = require('cors')
 
 express()
+  .use(cors())
   .use(express.static(path.join(__dirname, 'app')))
   .set('views', path.join(__dirname, 'app/views'))
   .set('view engine', 'ejs')
